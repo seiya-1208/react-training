96b716889c54f2cf1bcf29244dd4ed94d8e70477
seiya510 <www.dokomo.co.jp@gmail.com>
2021-04-23 20:14:41 +0900

    カード

--- src/pages/users/show.tsx
+++ src/pages/users/show.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 export const UserShowPage: FC = () => {
   const params = useParams<{ userId:string }>()
@@ -9,6 +13,43 @@ export const UserShowPage: FC = () => {
       <div>{ `UserShow: ${params.userId}` }</div>
       <div><Link to="/users">ユーザー一覧</Link></div>
       <div><Link to="/">トップ</Link></div>
+
+      <Box m={10} p={10}>
+      <Card>
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          せいや
+        </Typography>
+        <Typography variant="h5" component="h2">
+          男
+        </Typography>
+        <Typography color="textSecondary">
+          松原
+        </Typography>
+        <Typography variant="body2" component="p">
+          テスト
+        </Typography>
+      </CardContent>
+      </Card>
+      </Box>
+      <Box m={5} p={5} >
+      <Card>
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          コピー
+        </Typography>
+        <Typography variant="h5" component="h2">
+          した
+        </Typography>
+        <Typography color="textSecondary">
+          だけ
+        </Typography>
+        <Typography variant="body2" component="p">
+          スタイルテスト
+        </Typography>
+      </CardContent>
+      </Card>
+      </Box>
     </React.Fragment>
   )
 }
